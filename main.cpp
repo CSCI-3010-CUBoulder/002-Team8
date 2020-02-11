e7e1a0722666280e8f3dcb656c2ea9411536c6c9
Sihien Son <Sihienkson@cu-tcom-1-10.203.7.240.int.colorado.edu>
2020-02-11 11:31:57 -0700

    Changed the main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include "UtilityFunctions.h"
 
+int Factorial(int n){
+  int FacSum = 1;
+  if( n = 0){
+    return 1;
+  }
+  for (int i = 1; i < n; i++) {
+    FacSum = FacSum * i;
+  }
+}
+
+
+
 int main(){
 
 }
